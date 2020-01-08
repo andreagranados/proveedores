5878ac78f1e2088f574a1398448361dd5a0c4996
andrea <andrea@andrea-desktop>
2020-01-08 08:46:04 -0300

    Creo un perfil de consulta

---
+++ metadatos/permisos/apex_restriccion_funcional.sql
@@ -0,0 +1,13 @@
+
+------------------------------------------------------------
+-- apex_restriccion_funcional
+------------------------------------------------------------
+
+--- INICIO Grupo de desarrollo 0
+INSERT INTO apex_restriccion_funcional (proyecto, restriccion_funcional, descripcion, permite_edicion) VALUES (
+	'proveedores', --proyecto
+	'54', --restriccion_funcional
+	'consulta', --descripcion
+	'0'  --permite_edicion
+);
+--- FIN Grupo de desarrollo 0
---
+++ metadatos/permisos/apex_restriccion_funcional_evt.sql
@@ -0,0 +1,46 @@
+
+------------------------------------------------------------
+-- apex_restriccion_funcional_evt
+------------------------------------------------------------
+INSERT INTO apex_restriccion_funcional_evt (proyecto, restriccion_funcional, item, evento_id, no_visible) VALUES (
+	'proveedores', --proyecto
+	'54', --restriccion_funcional
+	'3833', --item
+	'3561', --evento_id
+	'1'  --no_visible
+);
+INSERT INTO apex_restriccion_funcional_evt (proyecto, restriccion_funcional, item, evento_id, no_visible) VALUES (
+	'proveedores', --proyecto
+	'54', --restriccion_funcional
+	'3833', --item
+	'3562', --evento_id
+	'1'  --no_visible
+);
+INSERT INTO apex_restriccion_funcional_evt (proyecto, restriccion_funcional, item, evento_id, no_visible) VALUES (
+	'proveedores', --proyecto
+	'54', --restriccion_funcional
+	'3833', --item
+	'3563', --evento_id
+	'1'  --no_visible
+);
+INSERT INTO apex_restriccion_funcional_evt (proyecto, restriccion_funcional, item, evento_id, no_visible) VALUES (
+	'proveedores', --proyecto
+	'54', --restriccion_funcional
+	'3833', --item
+	'3564', --evento_id
+	'1'  --no_visible
+);
+INSERT INTO apex_restriccion_funcional_evt (proyecto, restriccion_funcional, item, evento_id, no_visible) VALUES (
+	'proveedores', --proyecto
+	'54', --restriccion_funcional
+	'3833', --item
+	'3566', --evento_id
+	'1'  --no_visible
+);
+INSERT INTO apex_restriccion_funcional_evt (proyecto, restriccion_funcional, item, evento_id, no_visible) VALUES (
+	'proveedores', --proyecto
+	'54', --restriccion_funcional
+	'3833', --item
+	'3572', --evento_id
+	'1'  --no_visible
+);
